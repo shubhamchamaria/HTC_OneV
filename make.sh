2c5d4f50ec8120ceeec3c2a89c71314c829398d0
AdiPat <adithemagnificent@gmail.com>
2012-05-06 23:23:55 +0530

    My bad.

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 echo "~TITANIUM KERNEL COMPILER~"
 cd Kernel
-export ARCH=arm CROSS_COMPILE=/home/aditya/Toolchain/arm-eabi-linaro-4.6.2/bin/arm-eabi-
+export ARCH=arm CROSS_COMPILE=/home/aditya/Toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 echo "Cleaning up source"
 make clean && make mrproper
 echo "Enter your kernel version"
@@ -17,12 +17,14 @@ cp -f arch/arm/boot/zImage /home/aditya/HTC_OneV/zImage
 echo "Copying modules"
 cp Kernel/drivers/net/wireless/bcmdhd/bcmdhd.ko /home/aditya/HTC_OneV/Ramdisk/lib/modules/bcmdhd.ko
 cp Kernel/drivers/net/kineto_gan.ko /home/aditya/HTC_OneV/Ramdisk/lib/modules/kineto_gan.ko
+cp Kernel/fs/cifs/cifs.ko /home/aditya/HTC_OneV/Ramdisk/lib/modules/cifs.ko
+cp Kernel/drivers/net/tun.ko /home/aditya/HTC_OneV/Ramdisk/lib/modules/tun.ko
 cd /home/aditya/HTC_OneV/Ramdisk/lib/modules/
 
 for i in $(find . | grep .ko | grep './')
 do
         echo $i
-/home/aditya/Toolchain/arm-2009q3/bin/arm-none-eabi-strip --strip-unneeded $i
+/home/aditya/Toolchain/arm-2011.03/bin/arm-none-eabi-strip --strip-unneeded $i
 
 done
 
